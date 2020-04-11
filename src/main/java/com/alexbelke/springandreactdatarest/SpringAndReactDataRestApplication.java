ac1ceba59d80b17aef9d55063709d51ac906efd8
Alex Belke <alex.belke.de@gmail.com>
2020-04-11 09:44:00 +0200

    init commit

---
+++ src/main/java/com/alexbelke/springandreactdatarest/SpringAndReactDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.alexbelke.springandreactdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactDataRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexbelke/springandreactdatarest/SpringAndReactDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbelke.springandreactdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReactDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
