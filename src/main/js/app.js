f2d011e8f24b9f323ca090406eb8797ea0389f6f
Alex Belke <alex.belke.de@gmail.com>
2020-04-12 10:19:05 +0200

    add CreateDialog component

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -16,6 +16,15 @@ class App extends React.Component {
 		this.loadFromServer(this.state.pageSize);
 	}
 
+	render() {
+		return (
+			<div>
+				<CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+				<EmployeeList employees={this.state.employees}/>
+			</div>
+	)
+	}
+
 	loadFromServer(pageSize) {
 		follow(client, root, [
 			{rel: 'employees', params: {size: pageSize}}]
@@ -35,12 +44,6 @@ class App extends React.Component {
 				pageSize: pageSize,
 				links: employeeCollection.entity._links});
 		});
-	}
-
-	render() {
-		return (
-			<EmployeeList employees={this.state.employees}/>
-	)
 	}
 }
 
@@ -74,6 +77,59 @@ class Employee extends React.Component{
 			</tr>
 		)
 	}
+}
+
+class CreateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const newEmployee = {};
+		this.props.attributes.forEach(attribute => {
+			newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onCreate(newEmployee);
+
+		// clear out the dialog's inputs
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+		});
+
+		// Navigate away from the dialog to hide it.
+		window.location = "#";
+	}
+
+	render() {
+		const inputs = this.props.attributes.map(attribute =>
+			<p key={attribute}>
+				<input type="text" placeholder={attribute} ref={attribute} className="field"/>
+			</p>
+		);
+
+		return (
+			<div>
+				<a href="#createEmployee">Create</a>
+
+				<div id="createEmployee" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Create new employee</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>Create</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
 }
 
 ReactDOM.render(
